aa90f892edca3a16e29fd8cf634c5169c9a70eba
Xian Gabriel <xiangabpanganiban@gmail.com>
2026-08-29 13:01:11 +0800

    Completed the task in like 30 mins

--- q1/q1_sg5_a1_samat_panganiban.py
+++ q1/q1_sg5_a1_samat_panganiban.py
@@ -1,1 +1,25 @@
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
 
+    def take_damage(self, amount):
+        self.hp -= amount
+        print(f"Oh no! {self.name} took {amount} damage!")
+
+
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+
+
+def display_hp():
+    print(f"""
+{morgana.name}'s HP: {morgana.hp}
+{arthur.name}'s HP: {arthur.hp}
+""")
+
+display_hp()
+
+arthur.take_damage(10)
+
+display_hp()
